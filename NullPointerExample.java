b4742709255ee88ac246100f7e3a44a492a7151c
smithbwlhs <smithb@wlwv.k12.or.us>
2022-09-25 15:46:37 -0700

    created template for NullPointerExample.java

--- NullPointerExample.java
+++ NullPointerExample.java
@@ -5,21 +5,18 @@ public class NullPointerExample
     {
     
         // Declare our variables
-        Person realPerson;
-        Person ghost;
+        
     
         // Initialize realPerson
-        realPerson = new Person("Mike", 45);
-        System.out.println(realPerson);
+        
         
         // Initialize to a new Person, get a new memory address
-        realPerson = new Person("Michael", 45);
-        System.out.println(realPerson);
+        
         
         // ghost is never initialized, so there is no
         // memory address to point to.
         // Instead, the value held in the variable is null
-        System.out.println(ghost);
+        
         
     }
 }
