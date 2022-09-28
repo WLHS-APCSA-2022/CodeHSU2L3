3c2cca4269cb9c2d8117119e017bc36267835cd4
brandonsmithccc <smith.brandon7@student.clackamas.edu>
2022-09-28 09:07:26 -0700

    added some code
    made a tester class

--- Rectangle.java
+++ Rectangle.java
@@ -4,20 +4,33 @@ public class Rectangle
     private int width;
     private int height;
     
+
     /*Constructor
     Copies the values of rectWidth and rectHeight
     into width and height, respectively*/
- 
-    
+    public Rectangle(int rectWidth, int rectHeight){
+        width = rectWidth;
+        height = rectHeight;
+
+    }
+
     /* Constructor
     Allows user to construct a square
     Copies the value of sidelength
     into both width and height*/
+    public Rectangle(int sideLength){
+        width = sideLength;
+        height = sideLength;
+    }
 
     /*Default Constructor
     Allows a user to construct a rectangle
     without specifying dimensions*/
-    
+    public Rectangle(){
+        width = 0;
+        height = 0;
+    }
+
     // This method lets us print out the object
     // to see the values of the instance variables
     public String toString()
