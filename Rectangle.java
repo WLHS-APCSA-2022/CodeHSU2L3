9fbd444a03b7926079e8acfb5ee2fe8718768ba6
smithbwlhs <smithb@wlwv.k12.or.us>
2022-09-27 19:17:29 -0700

    added rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,27 @@
+public class Rectangle
+{
+    // Attributes
+    private int width;
+    private int height;
+    
+    /*Constructor
+    Copies the values of rectWidth and rectHeight
+    into width and height, respectively*/
+ 
+    
+    /* Constructor
+    Allows user to construct a square
+    Copies the value of sidelength
+    into both width and height*/
+
+    /*Default Constructor
+    Allows a user to construct a rectangle
+    without specifying dimensions*/
+    
+    // This method lets us print out the object
+    // to see the values of the instance variables
+    public String toString()
+    {
+        return "Rectangle with width: " + width + " and height: " + height;
+    }
+}
